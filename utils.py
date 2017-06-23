09516682b009bbb016d0fd8ad6cbf8695bdcd16f
Allan Zhou <allan.zhou@berkeley.edu>
2017-06-23 13:55:45 -0700

    Improved trajopt planning

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import interactpy
 import openravepy as orpy
 import numpy as np
 import pickle
+import json
+import trajoptpy
+from trajoptpy.check_traj import traj_is_safe
 
 
 dofs = np.array([-1.00000003e+00,
@@ -30,6 +33,7 @@ goal_T = np.array([[ 0.01297332,  0.99651909,  0.08234919,  1.21825334],
 
 def setup():
     env, robot = interactpy.initialize(saved_env='./default.env.xml')
+    robot.SetActiveDOFs(robot.arm.GetArmIndices())
     robot.SetTransform(robot_T)
     robot.SetDOFValues(dofs)
     mug = env.GetKinBody('mug')
@@ -37,3 +41,89 @@ def setup():
     viewer = env.GetViewer()
     viewer.SetCamera(camera_T)
     return env, robot
+
+
+def get_ik_solns(robot, goal_T):
+    with robot.GetEnv():
+        ik_param = orpy.IkParameterization(
+            goal_T, orpy.IkParameterizationType.Transform6D)
+        ik_solutions = robot.arm.FindIKSolutions(
+            ik_param, orpy.IkFilterOptions.CheckEnvCollisions,
+            ikreturn=False, releasegil=True)
+        return ik_solutions
+
+
+def waypoints_to_traj(env, robot, waypoints):
+    traj = orpy.RaveCreateTrajectory(env, '')
+    traj.Init(robot.GetActiveConfigurationSpecification('linear'))
+    for i in range(waypoints.shape[0]):
+        traj.Insert(i, waypoints[i])
+    return traj
+
+
+def trajopt_plan_to_ee(env, robot, goal_T, num_steps=10, num_inits=20):
+    local_dir = np.array([0, 0, 1])
+    arm_joints = [robot.GetJointFromDOFIndex(idx)
+                  for idx in robot.arm.GetArmIndices()]
+    def f(x):
+        robot.SetActiveDOFValues(x)
+        return robot.arm.hand.GetTransform()[:2,:3].dot(local_dir)
+    def dfdx(x):
+        robot.SetActiveDOFValues(x)
+        world_dir = robot.arm.hand.GetTransform()[:3,:3].dot(local_dir)
+        return np.array([np.cross(joint.GetAxis(), world_dir)[:2]
+                         for joint in arm_joints]).T.copy()
+
+    xyz = goal_T[:3,-1].tolist()
+    wxyz = orpy.quatFromRotationMatrix(goal_T[:3,:3]).tolist()
+    ik_solns = get_ik_solns(robot, goal_T)
+    np.random.shuffle(ik_solns)
+    for i in range(min(20, ik_solns.shape[0])):
+        init_joint_target = ik_solns[i].tolist()
+        request = {
+            "basic_info" : {
+                "n_steps" : num_steps,
+                "manip" : robot.GetName(),
+                "start_fixed" : True
+            },
+            "costs" : [
+                {
+                    "type" : "joint_vel",
+                    "params": {"coeffs" : [1]}
+                },
+                {
+                    "type" : "collision",
+                    "params" : {
+                        "coeffs" : [20],
+                        "dist_pen" : [0.025]
+                    }
+                }
+            ],
+            "constraints" : [
+                {
+                    "type" : "pose",
+                    "params" : {
+                        "xyz": xyz,
+                        "wxyz": wxyz,
+                        "link": robot.arm.hand.GetName(),
+                        "timestep": num_steps - 1
+                    }
+                }
+            ],
+            "init_info" : {
+                "type" : "straight_line",
+                "endpoint" : init_joint_target
+            }
+        }
+        s = json.dumps(request)
+        prob = trajoptpy.ConstructProblem(s, env)
+        for t in range(num_steps):
+            prob.AddConstraint(f, dfdx, [(t,j) for j in xrange(7)], "EQ", "up%i"%t)
+        result = trajoptpy.OptimizeProblem(prob)
+        is_safe = traj_is_safe(
+            result.GetTraj(), robot)
+        if is_safe:
+            return waypoints_to_traj(env, robot, result.GetTraj())
+    print('Failed to solve.')
+
+
