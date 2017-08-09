6ab182b8ddfd52dd0c64174419c49ab0732e63f3
Allan Zhou <allan.zhou@berkeley.edu>
2017-08-09 12:11:48 -0700

    Split up planning into 2 separate functions

--- planners.py
+++ planners.py
@@ -35,16 +35,50 @@ def feature_orientation(robot, x):
 featurizers = [feature_height, feature_orientation]
 
 
-def compute_feature_values(robot, waypoints):
-    result = {}
-    for f in featurizers:
-        total = 0
-        for i in range(waypoints.shape[0]):
-            old = robot.GetActiveDOFValues()
-            total += f(robot, waypoints[i])
-            robot.SetActiveDOFValues(old)
-        mean = total / waypoints.shape[0]
-        result[f.__name__] = mean
+def trajopt_simple_plan(env, robot, goal_config, num_steps=10, custom_costs={}, init=None):
+    start_joints = robot.GetActiveDOFValues()
+    if init is None:
+        init = mu.linspace2d(start_joints, goal_config, num_steps)
+    request = {
+        'basic_info' : {
+            'n_steps' : num_steps,
+            'manip' : robot.arm.GetName(),
+            'start_fixed' : True
+        },
+        'costs' : [
+            {
+                'type' : 'joint_vel',
+                'params': {'coeffs' : [1]}
+            },
+            {
+                'type' : 'collision',
+                'params' : {
+                    'coeffs' : [20],
+                    'dist_pen' : [0.025]
+                }
+            }
+        ],
+        'constraints' : [
+            {
+                'type' : 'joint',
+                'params' : {'vals': goal_config.tolist()}
+            }
+        ],
+        'init_info' : {
+            'type' : 'given_traj',
+            'data': init.tolist()
+        }
+    }
+    s = json.dumps(request)
+    prob = trajoptpy.ConstructProblem(s, env)
+    for t in range(num_steps):
+        for cost_name in custom_costs:
+            cost_fn = custom_costs[cost_name]
+            prob.AddCost(
+                cost_fn,
+                [(t,j) for j in range(7)],
+                '{:s}{:d}'.format(cost_name, t))
+    result = trajoptpy.OptimizeProblem(prob)
     return result
 
 
@@ -53,17 +87,13 @@ def trajopt_plan_to_config(env, robot, goal_config,
                            waypoints=[], duration=15,
                            retimer=None):
     def cost_height(x):
-        val = w[0] * feature_height(robot, x)
-        return val
+        return w[0] * feature_height(robot, x)
     def cost_orientation(x):
-        val = w[1] * feature_orientation(robot, x)
-        return val
-    def ee_timing_cost(waypoints):
-        waypoints = waypoints.reshape((7, -1)).T
-        ee_positions = np.stack([get_ee_coords(robot, x)
-                                 for x in waypoints])
-        ee_vel = np.square(np.diff(ee_positions, n=2, axis=0))
-        return np.sum(ee_vel)
+        return w[1] * feature_orientation(robot, x)
+    custom_costs = {
+        'height': cost_height,
+        'orientation': cost_orientation
+    }
     start_joints = robot.GetActiveDOFValues()
     inits = []
     inits.append(mu.linspace2d(start_joints, goal_config, num_steps))
@@ -73,69 +103,19 @@ def trajopt_plan_to_config(env, robot, goal_config,
     joint_target = goal_config.tolist()
     traj = None
     best_cost = np.inf
-    combined_feature_values = []
     init_unchanged_alert = None
     for i, init in enumerate(inits):
-        request = {
-            'basic_info' : {
-                'n_steps' : num_steps,
-                'manip' : robot.arm.GetName(),
-                'start_fixed' : True
-            },
-            'costs' : [
-                {
-                    'type' : 'joint_vel', # \sum_{t,j} (x_{t+1,j} - x_{t,j})^2
-                    'params': {'coeffs' : [1]}
-                },
-                {
-                    'type' : 'collision',
-                    'params' : {
-                        'coeffs' : [10 * 2],
-                        'dist_pen' : [0.025]
-                    }
-                }
-            ],
-            'constraints' : [
-                {
-                    'type' : 'joint',
-                    'params' : {'vals': joint_target}
-                }
-            ],
-            'init_info' : {
-                'type' : 'given_traj'
-            }
-        }
-        request['init_info']['data'] = init.tolist()
-        s = json.dumps(request)
-        prob = trajoptpy.ConstructProblem(s, env)
-        for t in range(num_steps):
-            prob.AddCost(
-                cost_height,
-                [(t,j) for j in xrange(7)],
-                'height%i'%t)
-            prob.AddCost(
-                cost_orientation,
-                [(t,j) for j in xrange(7)],
-                'orientation%i'%t)
-        # prob.AddCost(
-        #     ee_timing_cost,
-        #     [(t, j) for t in range(num_steps) for j in range(7)],
-        #     'ee_timing')
-        result = trajoptpy.OptimizeProblem(prob)
+        result = trajopt_simple_plan(
+            env,
+            robot,
+            goal_config,
+            num_steps=num_steps,
+            init=init,
+            custom_costs=custom_costs)
         waypoints = result.GetTraj()
         is_safe = traj_is_safe(
             waypoints, robot)
-        unzipped = zip(*result.GetCosts())
-        total_cost = np.sum(unzipped[1])
-        cost_names = list(unzipped[0])
-        collision_cost = sum([unzipped[1][k] for k in range(len(cost_names))
-                              if 'collision' in cost_names[k]])
-        smoothness_cost = unzipped[1][cost_names.index('joint_vel')]
-        feature_values = compute_feature_values(robot, waypoints)
-        feature_values['smoothness'] = smoothness_cost
-        feature_values['collision'] = collision_cost
-        feature_values['total'] = total_cost
-        combined_feature_values.append(feature_values)
+        total_cost = np.sum(zip(*result.GetCosts())[1])
         if total_cost < best_cost:
             traj = waypoints_to_traj(env, robot, waypoints, duration, retimer)
             best_cost = total_cost
@@ -145,4 +125,4 @@ def trajopt_plan_to_config(env, robot, goal_config,
                 init_unchanged_alert = None
     if init_unchanged_alert:
         print(init_unchanged_alert)
-    return traj, combined_feature_values
+    return traj
