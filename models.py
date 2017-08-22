8852ca9075f7635e822f99af3709263f4468d40d
Allan Zhou <allan.zhou@berkeley.edu>
2017-08-21 17:51:19 -0700

    Score->cost

--- models.py
+++ models.py
@@ -25,35 +25,37 @@ class MLP:
 class CostFunction:
     def __init__(self, load_path=None, num_wps=10, num_dofs=10, per_waypoint=False):
         self.sess = tf.Session()
+        self.waypoint_ph = tf.placeholder(tf.float32, shape=[None, num_dofs], name='waypoint_ph')
         self.trajA_ph = tf.placeholder(tf.float32, shape=[None, num_wps, num_dofs], name='trajA_ph')
         self.trajB_ph = tf.placeholder(tf.float32, shape=[None, num_wps, num_dofs], name='trajB_ph')
         self.label_ph = tf.placeholder(tf.int32, shape=[None], name='label_ph')
-        self.score_label_ph = tf.placeholder(tf.float32, shape=[None], name='score_label_ph')
+        self.cost_label_ph = tf.placeholder(tf.float32, shape=[None], name='cost_label_ph')
 
         batch_size = tf.shape(self.trajA_ph)[0]
         input_dim = num_dofs if per_waypoint else num_dofs * num_wps
         self.mlp = MLP(input_dim)
+        self.waypoint_cost = self.mlp(self.waypoint_ph)
         if per_waypoint:
-            A_scores, B_scores = [], []
+            A_costs, B_costs = [], []
             for wp in range(num_wps):
-                A_scores.append(self.mlp(self.trajA_ph[:,wp,:]))
-                B_scores.append(self.mlp(self.trajB_ph[:,wp,:]))
-            self.scoreA = tf.add_n(A_scores)
-            self.scoreB = tf.add_n(B_scores)
+                A_costs.append(self.mlp(self.trajA_ph[:,wp,:]))
+                B_costs.append(self.mlp(self.trajB_ph[:,wp,:]))
+            self.costA = tf.add_n(A_costs)
+            self.costB = tf.add_n(B_costs)
         else:
             trajA = tf.reshape(self.trajA_ph, [batch_size, input_dim])
             trajB = tf.reshape(self.trajB_ph, [batch_size, input_dim])
-            self.scoreA, self.scoreB = self.mlp(trajA), self.mlp(trajB)
+            self.costA, self.costB = self.mlp(trajA), self.mlp(trajB)
         # Probability proportional to e^{-cost}
-        score_logits = -1 * tf.concat([self.scoreA, self.scoreB], axis=-1)
+        cost_logits = -1 * tf.concat([self.costA, self.costB], axis=-1)
         self.loss = tf.reduce_mean(
             tf.nn.sparse_softmax_cross_entropy_with_logits(
-                logits=score_logits, labels=self.label_ph))
+                logits=cost_logits, labels=self.label_ph))
         # tf.summary.scalar('loss', self.loss)
         self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
-        self.score_loss = tf.reduce_mean(tf.square(self.scoreA - self.score_label_ph))
-        # tf.summary.scalar('score_loss', self.score_loss)
-        self.score_train_op = tf.train.AdamOptimizer().minimize(self.score_loss)
+        self.cost_loss = tf.reduce_mean(tf.square(self.costA - self.cost_label_ph))
+        # tf.summary.scalar('cost_loss', self.cost_loss)
+        self.cost_train_op = tf.train.AdamOptimizer().minimize(self.cost_loss)
 
         # self.summary_op = tf.merge_all_summaries()
         # self.train_writer = tf.summary.FileWriter()
@@ -65,13 +67,32 @@ class CostFunction:
         self.num_params = np.sum([np.prod(v.shape) for v in tf.get_collection(
             tf.GraphKeys.TRAINABLE_VARIABLES)])
 
-    def score(self, waypoints):
-        # Returns cost for a single trajectory with shape (num_wps, 7)
-        score = self.sess.run(self.scoreA, feed_dict={
-            self.trajA_ph: waypoints[None],
-            K.learning_phase(): False
+    def cost_waypoint(self, waypoint, learning_phase=False):
+        cost = self.sess.run(self.waypoint_cost, feed_dict={
+            self.waypoint_ph: waypoint[None],
+            K.learning_phase(): learning_phase
         })
-        return score[0][0]
+        return np.squeeze(cost)
+
+    def cost_traj(self, waypoints, learning_phase=False):
+        '''Returns cost for a single trajectory with shape (num_wps, 7).'''
+        cost = self.sess.run(self.costA, feed_dict={
+            self.trajA_ph: waypoints[None],
+            K.learning_phase(): learning_phase
+        })
+        return np.squeeze(cost)
+
+    def cost_traj_batch(self, trajs, learning_phase=False):
+        '''Returns cost for a batch of trajs with shape (num_trajs, num_wps, 7).'''
+        cost = self.sess.run(self.costA, feed_dict={
+            self.trajA_ph: trajs,
+            K.learning_phase(): learning_phase
+        })
+        return np.squeeze(cost)
+
+    def corrcoef(self, test_data, labels):
+        '''Correlation between model predictions and labels.'''
+        return np.corrcoef(self.cost_traj_batch(test_data), labels)[0,1]
 
     def train(self, trajsA, trajsB, labels):
         loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={
@@ -82,21 +103,21 @@ class CostFunction:
         })
         return loss
 
-    def pretrain_on_prior_cost(self, trajs, score_labels):
-        score_loss, _ = self.sess.run([self.score_loss, self.score_train_op], feed_dict={
+    def pretrain_on_prior_cost(self, trajs, cost_labels):
+        cost_loss, _ = self.sess.run([self.cost_loss, self.cost_train_op], feed_dict={
             self.trajA_ph: trajs,
-            self.score_label_ph: score_labels,
+            self.cost_label_ph: cost_labels,
             K.learning_phase(): True
         })
-        return score_loss
+        return cost_loss
 
-    def get_score_loss(self, trajs, score_labels):
-        score_loss = self.sess.run(self.score_loss, feed_dict={
+    def get_cost_loss(self, trajs, cost_labels):
+        cost_loss = self.sess.run(self.cost_loss, feed_dict={
             self.trajA_ph: trajs,
-            self.score_label_ph: score_labels,
+            self.cost_label_ph: cost_labels,
             K.learning_phase(): False
         })
-        return score_loss
+        return cost_loss
 
     def save_model(self, path):
         save_path = self.saver.save(self.sess, path)
