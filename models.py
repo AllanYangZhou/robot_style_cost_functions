a0b3883599bd67acdc4fe5cefdaea664aa365f55
Allan Zhou <allan.zhou@berkeley.edu>
2017-09-16 15:55:15 -0700

    LeakyReLU -> sigmoid

--- models.py
+++ models.py
@@ -10,11 +10,9 @@ class MLP:
     def __init__(self, input_dim, h_size=64):
         self.model = Sequential()
         self.model.add(Dense(h_size, input_dim=input_dim, activation='sigmoid'))
-        #self.model.add(LeakyReLU())
 
         self.model.add(Dropout(0.5))
         self.model.add(Dense(h_size, activation='sigmoid'))
-        #self.model.add(LeakyReLU())
 
         self.model.add(Dropout(0.5))
         self.model.add(Dense(1))
@@ -24,56 +22,52 @@ class MLP:
 
 
 class CostFunction:
-    def __init__(self, load_path=None, num_wps=10, num_dofs=10, per_waypoint=False, h_size=64):
+    def __init__(self, load_path=None, num_wps=10, num_dofs=10, h_size=64, add_total_time=False):
+        self.add_total_time = add_total_time
         self.sess = tf.Session()
-        self.waypoint_ph = tf.placeholder(tf.float32, shape=[None, num_dofs], name='waypoint_ph')
         self.trajA_ph = tf.placeholder(tf.float32, shape=[None, num_wps, num_dofs], name='trajA_ph')
         self.trajB_ph = tf.placeholder(tf.float32, shape=[None, num_wps, num_dofs], name='trajB_ph')
         self.label_ph = tf.placeholder(tf.int32, shape=[None], name='label_ph')
         self.cost_label_ph = tf.placeholder(tf.float32, shape=[None], name='cost_label_ph')
 
         batch_size = tf.shape(self.trajA_ph)[0]
-        input_dim = num_dofs if per_waypoint else num_dofs * num_wps
-        self.mlp = MLP(input_dim, h_size)
-        self.waypoint_cost = self.mlp(self.waypoint_ph) if per_waypoint else None
-        if per_waypoint:
-            A_costs, B_costs = [], []
-            for wp in range(num_wps):
-                A_costs.append(self.mlp(self.trajA_ph[:,wp,:]))
-                B_costs.append(self.mlp(self.trajB_ph[:,wp,:]))
-            self.costA = tf.add_n(A_costs)
-            self.costB = tf.add_n(B_costs)
-        else:
-            trajA = tf.reshape(self.trajA_ph, [batch_size, input_dim])
-            trajB = tf.reshape(self.trajB_ph, [batch_size, input_dim])
-            self.costA, self.costB = self.mlp(trajA), self.mlp(trajB)
+        input_dim = num_dofs * num_wps
+        self.mlp = MLP(input_dim + int(add_total_time), h_size)
+        trajA = tf.reshape(self.trajA_ph, [batch_size, input_dim])
+        trajB = tf.reshape(self.trajB_ph, [batch_size, input_dim])
+        if add_total_time:
+            self.trajA_time_ph = tf.placeholder(tf.float32, shape=[None, 1], name='trajA_time_ph')
+            self.trajB_time_ph = tf.placeholder(tf.float32, shape=[None, 1], name='trajB_time_ph')
+            trajA = tf.concat([trajA, self.trajA_time_ph], axis=1)
+            trajB = tf.concat([trajB, self.trajB_time_ph], axis=1)
+        self.costA, self.costB = self.mlp(trajA), self.mlp(trajB)
+
         # Probability proportional to e^{-cost}
         cost_logits = -1 * tf.concat([self.costA, self.costB], axis=-1)
         self.loss = tf.reduce_mean(
             tf.nn.sparse_softmax_cross_entropy_with_logits(
                 logits=cost_logits, labels=self.label_ph))
-        # tf.summary.scalar('loss', self.loss)
         self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
         self.cost_loss = tf.reduce_mean(tf.square(self.costA - self.cost_label_ph))
-        # tf.summary.scalar('cost_loss', self.cost_loss)
         self.cost_train_op = tf.train.AdamOptimizer().minimize(self.cost_loss)
 
-        # self.summary_op = tf.merge_all_summaries()
-        # self.train_writer = tf.summary.FileWriter()
+
+        # self.time_deltas = tf.Variable(10 * [1.0], 'time_deltas')
+        # self.time_reset = tf.assign(self.time_deltas, np.ones(10))
+        # self.world_f_ph = tf.placeholder(tf.float32, [10, 27])
+        # self.time_combined = tf.concat([self.time_deltas[:,None], self.world_f_ph], axis=1)
+        # self.time_loss = self.mlp(tf.reshape(self.time_combined, [1, num_wps * num_dofs]))
+        # self.inequalities = [self.time_deltas[i] for i in range(10)]
+        # self.time_optimizer = tf.contrib.opt.ScipyOptimizerInterface(
+        #     self.time_loss, var_to_bounds={self.time_deltas: (.1, 10.)},
+        #     var_list=[self.time_deltas])
+
         init_op = tf.global_variables_initializer()
-        self.saver = tf.train.Saver()
+        self.saver = tf.train.Saver(var_list=tf.trainable_variables())
         self.sess.run(init_op)
         if load_path:
             self.load_model(load_path)
-        self.num_params = np.sum([np.prod(v.shape) for v in tf.get_collection(
-            tf.GraphKeys.TRAINABLE_VARIABLES)])
-
-    def cost_waypoint(self, waypoint):
-        cost = self.sess.run(self.waypoint_cost, feed_dict={
-            self.waypoint_ph: waypoint[None],
-            K.learning_phase(): False
-        })
-        return np.squeeze(cost)
+        self.num_params = np.sum([np.prod(v.shape) for v in tf.trainable_variables()])
 
     def cost_traj(self, waypoints):
         '''Returns cost for a single trajectory with shape (num_wps, 7).'''
@@ -95,13 +89,17 @@ class CostFunction:
         '''Correlation between model predictions and labels.'''
         return np.corrcoef(self.cost_traj_batch(test_data), labels)[0,1]
 
-    def train(self, trajsA, trajsB, labels):
-        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={
+    def train(self, trajsA, trajsB, labels, total_time=(1,1)):
+        fd = {
             self.trajA_ph: trajsA,
             self.trajB_ph: trajsB,
             self.label_ph: labels,
             K.learning_phase(): True
-        })
+        }
+        if self.add_total_time:
+            fd[self.trajA_time_ph] = total_time[0]
+            fd[self.trajB_time_ph] = total_time[1]
+        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict=fd)
         return loss
 
     def pretrain_on_prior_cost(self, trajs, cost_labels):
@@ -122,7 +120,14 @@ class CostFunction:
 
     def save_model(self, path):
         save_path = self.saver.save(self.sess, path)
-        print('Model saved in file: {:s}'.format(save_path))
 
     def load_model(self, path):
         self.saver.restore(self.sess, path)
+
+    # def optimize_timing(self, world_features):
+    #     self.sess.run(self.time_reset)
+    #     self.time_optimizer.minimize(session=self.sess, feed_dict={
+    #         self.world_f_ph: world_features,
+    #         K.learning_phase(): False
+    #     })
+    #     return self.sess.run(self.time_deltas)
