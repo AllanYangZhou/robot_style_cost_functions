ac05c0a292bf18335a9bf370ac8bb0136c8617f9
Allan Zhou <allan.zhou@berkeley.edu>
2017-09-29 18:44:57 -0700

    Batched forward kinematics

--- forward_kinematics.py
+++ forward_kinematics.py
@@ -18,35 +18,44 @@ def openrave_get_fk_params(robot):
 
 
 def rodrigues(w_hat, theta):
-    return tf.eye(3) + tf.sin(theta) * w_hat + tf.matmul(w_hat, w_hat) * (1-tf.cos(theta))
+    t1 = tf.eye(3)
+    t2 = tf.sin(theta)[:,None,None] * w_hat[None]
+    t3 = (1-tf.cos(theta))[:,None,None] * tf.matmul(w_hat, w_hat)[None]
+    return t1 + t2 + t3
 
 
 def make_homog(rot, trans):
-    top = tf.concat([rot, trans], axis=1)
-    return tf.concat([top, tf.convert_to_tensor([[0., 0, 0, 1]])], axis=0)
+    top = tf.concat([rot, trans], axis=2)
+    batch_size = tf.shape(rot)[0]
+    bottom = tf.convert_to_tensor([[0., 0, 0, 1]])
+    bottom = tf.tile(bottom[None], [batch_size, 1, 1])
+    return tf.concat([top, bottom], axis=1)
 
 
 def top_right(rot, w, v, theta):
-    term1 = tf.matmul(tf.eye(3) - rot, tf.transpose(tf.cross(w, v)[None]))
+    batch_size = tf.shape(rot)[0]
+    w_cross_v = tf.tile(tf.transpose(tf.cross(w, v)[None])[None], [batch_size, 1, 1])
+    term1 = tf.matmul(tf.eye(3) - rot, w_cross_v)
     wwT = tf.matmul(tf.transpose(w[None]), w[None])
-    term2 = theta * tf.matmul(wwT, tf.transpose(v[None]))
+    term2 = theta[:,None,None] * tf.matmul(wwT, tf.transpose(v[None]))[None]
     return term1 + term2
 
 
 def forward_kinematics(configs, g0s, axes, anchors):
+    batch_size = tf.shape(configs)[0]
     num_links = len(axes)
     results = []
-    g_1 = tf.constant(np.eye(4).astype(np.float32))
+    g_1 = tf.tile(tf.eye(4)[None], [batch_size, 1, 1])
+    I3 = tf.eye(3)
     for i in range(num_links):
-        g0 = g0s[i]
+        g0 = tf.tile(g0s[i][None], [batch_size, 1, 1])
         w = axes[i]
         q = anchors[i]
         v = -1 * tf.cross(w, q)
-        w_hat = tf.convert_to_tensor([[0, -w[2], w[1]],
-                                      [w[2], 0, -w[0]],
-                                      [-w[1], w[0], 0]])
-        rot = rodrigues(w_hat, configs[i])
-        tr = top_right(rot, w, v, configs[i])
+        tiled_w = tf.reshape(tf.tile(w, [3]), [3, 3])
+        w_hat = tf.transpose(tf.cross(tiled_w, I3))
+        rot = rodrigues(w_hat, configs[:,i])
+        tr = top_right(rot, w, v, configs[:,i])
         homog = make_homog(rot, tr)
         g_1 = tf.matmul(g_1, homog)
         res = tf.matmul(g_1, g0)
